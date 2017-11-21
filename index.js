37bd55319b7e2dce2044c7eb3fa672051ac2aae7
Thomas Jascur <tjascur@yahoo.com>
2017-11-21 06:29:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,7 @@ function printBadges(employee){
 
 
 
-// function tailsNeverFails() {
-//   return Math.random() >= 0.5;
-// }
-//
-// let j = 0;
-//
-// while (tailsNeverFails()) {
-//   j++;
-// }
-// console.log(`You got ${j} tails in a row!`)
+
 
 function tailsNeverFails() {
   let j = 0;
