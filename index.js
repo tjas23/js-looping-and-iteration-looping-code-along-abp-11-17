672009b195b0ce43b39dd64d5fff159a9c79d239
Thomas Jascur <tjascur@yahoo.com>
2017-11-21 05:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 const gifts = ['teddy bear', 'drone', 'doll'];
+
 function wrapGifts(gifts){
   for (let i = 0; i < gifts.length; i++){
     console.log(`I wrapped ${gifts[i]} and added a bow!`);
