0d48144e92b72326c21a8073570f042a9ee65a1e
Thomas Jascur <tjascur@yahoo.com>
2017-11-21 05:23:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function printBadges(emplyee){
   for (let i = 0; i < employee.length; i++){
     console.log(`Welcome ${employee[i]}! You are employee #${i}.`)
   }
-  return employee[];
+  return employee;
 }
