ffd7fa25dc335ac0a634ee604586fd208b9c2ecf
Thomas Jascur <tjascur@yahoo.com>
2017-11-21 06:29:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+//const employee = ['Ada', 'Brendan', 'Ali'];
+
+function printBadges(employee){
+  for (let i = 0; i < employee.length; i++){
+    console.log(`Welcome ${employee[i]}! You are employee #${i+1}.`);
+  }
+  return employee;
+}
+
+
+
+
+
+function tailsNeverFails() {
+  let j = 0;
+  while (Math.random() >= 0.5) {
+    j++
+//    console.log(j)
+  }
+  return `You got ${j} tails in a row!`
+}
