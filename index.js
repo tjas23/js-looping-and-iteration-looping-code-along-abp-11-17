18109fb50474bc94103bf50e0026fbcfa354757f
Thomas Jascur <tjascur@yahoo.com>
2017-11-21 05:20:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solutions in this file
-const employees = ['Ada', 'Brendan', 'Ali'];
+const employee = ['Ada', 'Brendan', 'Ali'];
 
-function printBadges(emplyees){
-  for (let i = 0; i < employees.length; i++){
+function printBadges(emplyee){
+  for (let i = 0; i < employee.length; i++){
     console.log(`Welcome ${employee[i]}! You are employee #${i} `)
   }
-  return employees
+  return employee;
 }
