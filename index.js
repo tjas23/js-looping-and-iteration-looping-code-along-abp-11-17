b7c7b4256454196471775f54c15c16e58945b7e0
Thomas Jascur <tjascur@yahoo.com>
2017-11-21 05:32:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 function printBadges(employee){
   for (let i = 0; i < employee.length; i++){
-    console.log(`Welcome ${employee[i]}! You are employee #${i}.`);
+    console.log(`Welcome ${employee[i]}! You are employee #${i+1}.`);
   }
   return employee;
 }
